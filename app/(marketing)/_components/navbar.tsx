9f617fe53e3abc04438e235350b0ed57a3f43f10
ssarandev <samuil.sarandev@dxc.com>
2023-12-21 12:10:15 +0200

    Add: add fixed navbar component with custom useScrollTop hook

---
+++ app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useScrollTop } from '@/hooks/use-scoll-top';
+import { cn } from '@/lib/utils';
+import Logo from './logo';
+import { ModeToggle } from '@/components/mode-toggle';
+
+const NavBar = () => {
+	const scrolled = useScrollTop();
+
+	return (
+		<div
+			className={cn(
+				'z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6',
+				scrolled && 'border-b shadow-sm'
+			)}
+		>
+			<Logo />
+			<div className='md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2'>
+				<ModeToggle />
+			</div>
+		</div>
+	);
+};
+
+export default NavBar;
