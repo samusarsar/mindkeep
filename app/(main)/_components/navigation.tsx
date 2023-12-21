da3ede4b1e2e91152bd8981cd3eafd92385e27ea
samu.sar <samuil.sarandev@gmail.com>
2023-12-21 16:37:07 +0200

    Add: add user-item to navigation component

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,6 +6,7 @@ import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
 
 import { cn } from '@/lib/utils';
+import UserItem from './user-item';
 
 const Navigation = () => {
 	const pathname = usePathname();
@@ -120,7 +121,7 @@ const Navigation = () => {
 					<ChevronsLeft className='h-6 w-6' />
 				</div>
 				<div>
-					<p>Action items</p>
+					<UserItem />
 				</div>
 				<div className='mt-4'>
 					<p>Documents</p>
