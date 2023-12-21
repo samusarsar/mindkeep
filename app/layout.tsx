ad63194bde2ce236569af3cd03d066d0e1541c04
samu.sar <samuil.sarandev@gmail.com>
2023-12-21 13:51:40 +0200

    Setup: finalize convex setup with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/providers/theme-provider';
+import { ConvexClientProvider } from '@/components/providers/convex-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -36,15 +37,17 @@ export default function RootLayout({
 			suppressHydrationWarning
 		>
 			<body className={inter.className}>
-				<ThemeProvider
-					attribute='class'
-					defaultTheme='system'
-					enableSystem
-					disableTransitionOnChange
-					storageKey='mindkeep-theme'
-				>
-					{children}
-				</ThemeProvider>
+				<ConvexClientProvider>
+					<ThemeProvider
+						attribute='class'
+						defaultTheme='system'
+						enableSystem
+						disableTransitionOnChange
+						storageKey='mindkeep-theme'
+					>
+						{children}
+					</ThemeProvider>
+				</ConvexClientProvider>
 			</body>
 		</html>
 	);
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+	providers: [
+		{
+			domain: 'https://classic-locust-35.clerk.accounts.dev',
+			applicationID: 'convex',
+		},
+	],
+};
