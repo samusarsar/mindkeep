52fa2e9aa2846b460ca44ae65f015a9571a89433
samu.sar <samuil.sarandev@gmail.com>
2023-12-21 14:14:03 +0200

    Add: add authentication flow with clerk and conditional rendering to navbar and heading

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,16 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
+import { useConvexAuth } from 'convex/react';
 import { ArrowRight } from 'lucide-react';
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/spinner';
+import { SignInButton } from '@clerk/clerk-react';
 
 const Heading = () => {
+	const { isAuthenticated, isLoading } = useConvexAuth();
+
 	return (
 		<div className='max-w-3xl space-y-4'>
 			<h1 className='text-3xl sm:text-5xl md:text-6xl font-bold'>
@@ -14,9 +21,26 @@ const Heading = () => {
 				Mindkeep is the connected environment and workspace for <br />
 				new-age mindkeeping.
 			</h3>
-			<Button>
-				Enter Mindkeep <ArrowRight className='h-4 w-4 ml-2' />
-			</Button>
+			{isLoading && (
+				<div className='w-full flex items-center justify-center'>
+					<Spinner size='lg' />
+				</div>
+			)}
+			{isAuthenticated && !isLoading && (
+				<Button asChild>
+					<Link href='/documents'>
+						Enter Mindkeep <ArrowRight className='h-4 w-4 ml-2' />
+					</Link>
+				</Button>
+			)}
+			{!isAuthenticated && !isLoading && (
+				<SignInButton mode='modal'>
+					<Button>
+						Get Mindkeep for free
+						<ArrowRight className='h-4 w-4 ml-2' />
+					</Button>
+				</SignInButton>
+			)}
 		</div>
 	);
 };
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,11 +1,19 @@
 'use client';
 
+import { useConvexAuth } from 'convex/react';
+import { SignInButton, UserButton } from '@clerk/clerk-react';
+import Link from 'next/link';
+
 import { useScrollTop } from '@/hooks/use-scoll-top';
-import { cn } from '@/lib/utils';
-import Logo from './logo';
 import { ModeToggle } from '@/components/mode-toggle';
+import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/spinner';
+import { cn } from '@/lib/utils';
+
+import Logo from './logo';
 
 const NavBar = () => {
+	const { isAuthenticated, isLoading } = useConvexAuth();
 	const scrolled = useScrollTop();
 
 	return (
@@ -17,6 +25,34 @@ const NavBar = () => {
 		>
 			<Logo />
 			<div className='md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2'>
+				{isLoading && <Spinner />}
+				{!isAuthenticated && !isLoading && (
+					<>
+						<SignInButton mode='modal'>
+							<Button
+								variant={'ghost'}
+								size={'sm'}
+							>
+								Log in
+							</Button>
+						</SignInButton>
+						<SignInButton mode='modal'>
+							<Button size={'sm'}>Get Mindkeep free</Button>
+						</SignInButton>
+					</>
+				)}
+				{isAuthenticated && !isLoading && (
+					<>
+						<Button
+							variant='ghost'
+							size='sm'
+							asChild
+						>
+							<Link href='/documents'>Enter Mindkeep</Link>
+						</Button>
+						<UserButton afterSignOutUrl='/' />
+					</>
+				)}
 				<ModeToggle />
 			</div>
 		</div>
