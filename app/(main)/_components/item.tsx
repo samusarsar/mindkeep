5ad3892f1b2146652fb7ee6a147612a34a9e6e55
samu.sar <samuil.sarandev@gmail.com>
2023-12-22 14:58:57 +0200

    Add: add ItemSkeleton to Item component

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -4,6 +4,7 @@ import { LucideIcon, ChevronRight, ChevronDown } from 'lucide-react';
 
 import { Id } from '@/convex/_generated/dataModel';
 import { cn } from '@/lib/utils';
+import { Skeleton } from '@/components/ui/skeleton';
 
 interface ItemProps {
 	id?: Id<'documents'>;
@@ -62,6 +63,20 @@ const Item = ({
 					<span className='text-xs'>⌘</span>K
 				</kbd>
 			)}
+		</div>
+	);
+};
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+	return (
+		<div
+			style={{
+				paddingLeft: level ? `${level * 12 + 25}px` : '12px',
+			}}
+			className='flex gap-x-2 py-[3px]'
+		>
+			<Skeleton className='h-4 w-4' />
+			<Skeleton className='h-4 w-[30%]' />
 		</div>
 	);
 };
