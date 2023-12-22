7f2b9a7c031ad3511ce57525bf1e68dc5a271323
samu.sar <samuil.sarandev@gmail.com>
2023-12-22 15:38:27 +0200

    Add: add nested expanding and document creation functionality to item component

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,8 +1,12 @@
 'use client';
 
-import { LucideIcon, ChevronRight, ChevronDown } from 'lucide-react';
+import { LucideIcon, ChevronRight, ChevronDown, Plus } from 'lucide-react';
+import { useMutation } from 'convex/react';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 
 import { Id } from '@/convex/_generated/dataModel';
+import { api } from '@/convex/_generated/api';
 import { cn } from '@/lib/utils';
 import { Skeleton } from '@/components/ui/skeleton';
 
@@ -31,6 +35,37 @@ const Item = ({
 	onExpand,
 	expanded,
 }: ItemProps) => {
+	const router = useRouter();
+	const create = useMutation(api.documents.create);
+
+	const handleExpand = (
+		event: React.MouseEvent<HTMLDivElement, MouseEvent>
+	) => {
+		event.stopPropagation();
+		onExpand?.();
+	};
+
+	const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		event.stopPropagation();
+
+		if (!id) return;
+
+		const promise = create({ title: 'Untitled', parentDocument: id }).then(
+			(documentId) => {
+				if (!expanded) {
+					onExpand?.();
+				}
+				router.push(`/documents/${documentId}`);
+			}
+		);
+
+		toast.promise(promise, {
+			loading: 'Creating a new note...',
+			success: 'New note created!',
+			error: 'Failed to create a new note.',
+		});
+	};
+
 	const ChevronIcon = expanded ? ChevronDown : ChevronRight;
 
 	return (
@@ -47,7 +82,7 @@ const Item = ({
 				<div
 					role='button'
 					className='h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1'
-					onClick={() => {}}
+					onClick={handleExpand}
 				>
 					<ChevronIcon className='h-4 w-4 shrink-0 text-muted-foreground/50' />
 				</div>
@@ -62,6 +97,17 @@ const Item = ({
 				<kbd className='ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 roounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100'>
 					<span className='text-xs'>⌘</span>K
 				</kbd>
+			)}
+			{!!id && (
+				<div className='ml-auto flex items-center gap-x-2'>
+					<div
+						role='button'
+						onClick={onCreate}
+						className='opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600'
+					>
+						<Plus className='h-4 w-4 text-muted-foreground' />
+					</div>
+				</div>
 			)}
 		</div>
 	);
