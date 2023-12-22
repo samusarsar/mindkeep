0f49b29a767f04ae53436a3e48e15ee077fb32c7
samu.sar <samuil.sarandev@gmail.com>
2023-12-22 11:08:10 +0200

    Add: add create note handler to documents page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -3,11 +3,27 @@
 import Image from 'next/image';
 import { useUser } from '@clerk/clerk-react';
 import { PlusCircle } from 'lucide-react';
+import { useMutation } from 'convex/react';
+import { toast } from 'sonner';
 
+import { api } from '@/convex/_generated/api';
 import { Button } from '@/components/ui/button';
 
 const DocumentsPage = () => {
 	const { user } = useUser();
+	const create = useMutation(api.documents.create);
+
+	const handleCreate = () => {
+		const promise = create({
+			title: 'Untitled',
+		});
+
+		toast.promise(promise, {
+			loading: 'Creating a new note...',
+			success: 'New note created!',
+			error: 'Failed to create a new note.',
+		});
+	};
 
 	return (
 		<div className='h-full flex flex-col items-center justify-center space-y-4'>
@@ -16,6 +32,7 @@ const DocumentsPage = () => {
 				height='300'
 				width='300'
 				alt='Empty'
+				priority
 				className='dark:hidden'
 			/>
 			<Image
@@ -23,12 +40,13 @@ const DocumentsPage = () => {
 				height='300'
 				width='300'
 				alt='Empty'
+				priority
 				className='hidden dark:block'
 			/>
 			<h2 className='text-lg font-medium'>
 				Welcome to {user?.firstName}&apos;s Mindkeep
 			</h2>
-			<Button>
+			<Button onClick={handleCreate}>
 				<PlusCircle className='h-4 w-4 mr-2' /> Create a note
 			</Button>
 		</div>
