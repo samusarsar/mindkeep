61b07e76c51ccc65a885b4f9c831284eb76091a0
samu.sar <samuil.sarandev@gmail.com>
2024-01-02 15:47:59 +0200

    Add: add menu component to document view to include on-page actions

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -9,6 +9,7 @@ import { Id } from '@/convex/_generated/dataModel';
 
 import Title from './title';
 import Banner from './banner';
+import Menu from './menu';
 
 interface NavbarProps {
 	isCollapsed: boolean;
@@ -24,8 +25,11 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
 	if (document === undefined) {
 		return (
-			<nav className='bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center'>
+			<nav className='bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between'>
 				<Title.Skeleton />
+				<div className='flex items-center gap-x-2'>
+					<Menu.Skeleton />
+				</div>
 			</nav>
 		);
 	}
@@ -46,6 +50,9 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 				)}
 				<div className='flex items-center justify-between w-full'>
 					<Title initialData={document} />
+					<div className='flex items-center gap-x-2'>
+						<Menu documentId={document._id} />
+					</div>
 				</div>
 			</nav>
 			{document.isArchived && <Banner documentId={document._id} />}
