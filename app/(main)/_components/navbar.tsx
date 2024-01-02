3b4e5b19951ac8e2ea872664bf9c2f4c2dfa9856
samu.sar <samuil.sarandev@gmail.com>
2024-01-02 15:30:57 +0200

    Add: add banner component with restore and remove functionality for items in trash box

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -2,11 +2,13 @@
 
 import { useQuery } from 'convex/react';
 import { useParams } from 'next/navigation';
+import { MenuIcon } from 'lucide-react';
 
 import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
-import { MenuIcon } from 'lucide-react';
+
 import Title from './title';
+import Banner from './banner';
 
 interface NavbarProps {
 	isCollapsed: boolean;
@@ -33,18 +35,21 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 	}
 
 	return (
-		<nav className='bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4'>
-			{isCollapsed && (
-				<MenuIcon
-					role='button'
-					onClick={onResetWidth}
-					className='h-6 w-6 text-muted-foreground'
-				/>
-			)}
-			<div className='flex items-center justify-between w-full'>
-				<Title initialData={document} />
-			</div>
-		</nav>
+		<>
+			<nav className='bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4'>
+				{isCollapsed && (
+					<MenuIcon
+						role='button'
+						onClick={onResetWidth}
+						className='h-6 w-6 text-muted-foreground'
+					/>
+				)}
+				<div className='flex items-center justify-between w-full'>
+					<Title initialData={document} />
+				</div>
+			</nav>
+			{document.isArchived && <Banner documentId={document._id} />}
+		</>
 	);
 };
 
