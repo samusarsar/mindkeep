ec37739b868aa29c09372f37d4645debfc2815bf
samu.sar <samuil.sarandev@gmail.com>
2024-01-03 12:54:10 +0200

    Add: add layout and page for preview document routes

---
+++ app/(public)/layout.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div className='h-full flex dark:bg-[#1F1F1F]'>
+			<main className='flex-1 h-full overflow-y-auto'>{children}</main>
+		</div>
+	);
+};
+
+export default MainLayout;
