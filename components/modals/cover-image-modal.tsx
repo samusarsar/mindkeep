c7add2e92739946c54fc6a40f1c54386793063a2
samu.sar <samuil.sarandev@gmail.com>
2024-01-03 10:50:09 +0200

    Add: extend cover image modal and disclosure store to allow for image removal and replacing

--- components/modals/cover-image-modal.tsx
+++ components/modals/cover-image-modal.tsx
@@ -33,6 +33,9 @@ const CoverImageModal = () => {
 
 			const res = await edgestore.publicFiles.upload({
 				file,
+				options: {
+					replaceTargetUrl: coverImage.url,
+				},
 			});
 
 			await update({
